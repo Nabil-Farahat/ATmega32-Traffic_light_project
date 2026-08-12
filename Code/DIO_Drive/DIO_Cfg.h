b6659587a52dee88240135a9e95c33ffe2543bfb
Nabil-Farahat <nabillabloq@gmail.com>
2026-08-12 06:19:37 +0300

    Add files via upload

---
+++ Code/DIO_Drive/DIO_Cfg.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_Cfg.h
+ *
+ *  Created on: Aug 8, 2026
+ *      Author: DeLL
+ */
+
+#ifndef DIO_CFG_H_
+#define DIO_CFG_H_
+
+
+
+#endif /* DIO_CFG_H_ */
---
+++ Code/DIO_Drive/DIO_Prv.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_Prv.h
+ *
+ *  Created on: Aug 8, 2026
+ *      Author: DeLL
+ */
+
+#ifndef DIO_PRV_H_
+#define DIO_PRV_H_
+
+
+
+#endif /* DIO_PRV_H_ */
